1dda3130c771e41d68a6664fae6cdb4091fc41e9
Miguel <mige.d@outlook.com>
2021-11-01 17:44:05 -0400

    refactor input

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (data) => handleUserInput(data));
+
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('exiting..');
+    process.exit();
+  }
+};
+
+module.exports = { setupInput, handleUserInput };
--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const { connect } = require('./client');
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (data) => handleUserInput(data));
-
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    console.log('exiting..');
-    process.exit();
-  }
-};
-
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
